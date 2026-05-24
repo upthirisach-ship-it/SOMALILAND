b053f43cc1d74e80847f56fa5d8a007e85266763
upthirisach-ship-it <upthirisach@gmail.com>
2026-05-24 05:23:32 -0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,23 @@
+{
+  "name": "whatsapp-bot",
+  "version": "1.0.0",
+  "description": "Simple WhatsApp bot using Baileys (Node.js)",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "node index.js"
+  },
+  "keywords": [
+    "whatsapp",
+    "bot",
+    "baileys",
+    "nodejs",
+    "automation"
+  ],
+  "author": "your-name",
+  "license": "MIT",
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.8",
+    "qrcode-terminal": "^0.12.0"
+  }
+}
